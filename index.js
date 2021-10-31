1184ebe6ce2e07d148463341dff34e1106020d42
Rayhan <rayhanchy119gmail.com>
2021-10-31 15:39:25 +0600

    added again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 //port declare
-const port = 5000;
+const port = process.env.PORT || 5000;
 //add monogodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.be8dg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
